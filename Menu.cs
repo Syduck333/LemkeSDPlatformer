b8c298226455fd0653670d94ab3aac53dca4f461
Syduck333 <127541086+Syduck333@users.noreply.github.com>
2026-09-11 22:49:32 -0400

    Menu Basics
    Added game name and button. Scripting not done.

---
+++ Menu.cs
@@ -0,0 +1,19 @@
+using Godot;
+using System;
+
+public partial class Menu : Control
+{
+	public string LevelOne = "res://level.tscn";
+	private Button _Buttonplay;
+	
+	
+	
+	public override void _Ready()
+	{
+	}
+
+	
+	public override void _Process(double delta)
+	{
+	}
+}
